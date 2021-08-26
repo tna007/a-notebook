236bc04a3a0c1e0241898b4690c9f3d70b97de10
tna007 <anh.to@edu.bc.fi>
2021-08-26 12:31:04 +0300

    Update data.json

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "./reducer";
+import thunk from "redux-thunk";
 
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
